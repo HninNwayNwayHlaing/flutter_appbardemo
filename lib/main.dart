3d3794ffeb8edabb0cd682f801919f60f6e28f1c
Hnin Nway Nway Hlaing <nwaynway@pixeldirects.com>
2018-04-11 16:35:30 +0630

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String _title = "App Bar Demo";
+  String _myState = "No State";
+
+  void _Pressed(String message) {
+    print(message);
+    setState(() {
+      _myState = message;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: _title,
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text(_title),
+          actions: <Widget>[
+            new IconButton(
+                icon: new Icon(Icons.add_alert),
+                onPressed: () => _Pressed('Alert Pressed')),
+            new IconButton(
+                icon: new Icon(Icons.print),
+                onPressed: () => _Pressed('Print Pressed')),
+            new IconButton(
+                icon: new Icon(Icons.people),
+                onPressed: () => _Pressed('People Pressed')),
+            new RaisedButton(
+              onPressed: null,
+              child: new Text("Press Me"),
+              color: Colors.white,
+              splashColor: Colors.blue,
+            ),
+          ],
+        ),
+        body: new Container(
+          padding: const EdgeInsets.all(32.0),
+          child: new Center(
+            child: new Text(_myState),
+          ),
+        ),
+      ),
+    );
+  }
+}
